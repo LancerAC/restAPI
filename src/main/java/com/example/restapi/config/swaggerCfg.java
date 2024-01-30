a96e8f43d00fe85bb8f0a37f47640a6152498c93
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-30 18:12:12 +0300

    create swaggerCfg

---
+++ src/main/java/com/example/restapi/config/swaggerCfg.java
@@ -0,0 +1,27 @@
+package com.example.restapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class swaggerCfg {
+
+    @Bean
+    public OpenAPI api(){
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                )).info(
+                        new Info().title("Our car api")
+                );
+
+    }
+
+}
