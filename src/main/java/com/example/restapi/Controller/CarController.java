661b65c3c73721dd739e3c82e1a975de7acfc0b7
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-30 17:32:37 +0300

    refactor CarController: remove object mapper, add methods for get and add cars

--- src/main/java/com/example/restapi/Controller/CarController.java
+++ src/main/java/com/example/restapi/Controller/CarController.java
@@ -1,15 +1,23 @@
 package com.example.restapi.Controller;
 
 import com.example.restapi.Entity.Car;
+import com.example.restapi.repository.CarRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class CarController {
     @GetMapping("/main")
     public String mainListener(){
@@ -17,26 +25,15 @@ public class CarController {
     }
 
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final CarRepository carRepository;
 
-    private Car createCar(){
-        Car car = Car.builder()
-                .brand("UAZ")
-                .weight(2344)
-                .build();
 
-        return car;
+    @PostMapping("/add")
+    public void addCar(@RequestBody Car car){
+        log.info("Car created: " + carRepository.save(car));
     }
-
-    @GetMapping("/get")
-    @SneakyThrows
-    public String getCar(){
-
-        @NonNull
-        String jsonData = objectMapper.writeValueAsString(createCar());
-
-        return jsonData;
-
+    @GetMapping("/getAll")
+    public List<Car> getAllCars(){
+        return carRepository.findAll();
     }
 }
