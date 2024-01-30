32b0eeafd2bb72d33ec5e5dfe30b263137ee3fc5
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-30 17:11:38 +0300

    Create method for serialization car

--- src/main/java/com/example/restapi/Controller/CarController.java
+++ src/main/java/com/example/restapi/Controller/CarController.java
@@ -1,6 +1,12 @@
 package com.example.restapi.Controller;
 
+import com.example.restapi.Entity.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +14,29 @@ public class CarController {
     @GetMapping("/main")
     public String mainListener(){
         return "Welcome!";
+    }
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Car createCar(){
+        Car car = Car.builder()
+                .brand("UAZ")
+                .weight(2344)
+                .build();
+
+        return car;
+    }
+
+    @GetMapping("/get")
+    @SneakyThrows
+    public String getCar(){
+
+        @NonNull
+        String jsonData = objectMapper.writeValueAsString(createCar());
+
+        return jsonData;
+
     }
 }
