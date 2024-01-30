dee9d195498ba4503d748ee4e29d8df650863402
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-30 17:58:03 +0300

    create method getCarById

--- src/main/java/com/example/restapi/Controller/CarController.java
+++ src/main/java/com/example/restapi/Controller/CarController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +38,13 @@ public class CarController {
     @GetMapping("/getAll")
     public List<Car> getAllCars(){
         return carRepository.findAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public Car getCarById(@PathVariable Long id){
+        return carRepository.findById(id)
+                .orElseThrow(()
+                        -> new RuntimeException(
+                                "Not found"));
     }
 }
